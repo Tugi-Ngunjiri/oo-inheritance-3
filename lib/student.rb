9b625f956bffbdd025ebbb6a82aea2f103a8d2ea
Tugi Ngunjiri <102291617+Tugi-Ngunjiri@users.noreply.github.com>
2023-05-19 17:40:40 +0300

    Initial commit

---
+++ lib/student.rb
@@ -0,0 +1,10 @@
+require_relative './user'
+class Student < User
+    attr_accessor :knowledge
+    def initialize
+        @knowledge=[]
+    end
+    def learn lesson
+        @knowledge << lesson
+    end
+end
